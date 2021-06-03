b76b719df4b5ef553e1b0118ebd10e7b588ede8f
Amr Ehab <amrabdulaziz98@gmail.com>
2021-06-03 11:58:49 +0200

    lcd.c and lcd.h init

---
+++ lcd/LCD.c
@@ -0,0 +1,8 @@
+#include "LCD.h"
+
+void LCD_Init(void){     
+}
+
+void LCD_OutChar(unsigned char data){
+                 
+}
---
+++ lcd/LCD.h
@@ -0,0 +1,8 @@
+#ifndef LCD_H
+#define LCD_H
+#include "../tm4c123gh6pm.h"
+
+void LCD_Init(void);
+void LCD_OutChar(unsigned char data);
+
+#endif
