37746772bc053f5c192c29e5e7bc8e8b743fdcc5
Omar Rashed <70315665+OmarRash@users.noreply.github.com>
2021-06-04 15:55:28 +0200

    Distance Calculation: disatnce files init

---
+++ distance_calculation/DistanceCalculation.c
@@ -0,0 +1,11 @@
+#include "DistanceCalculation.h"
+
+long double to_rad(long double degree){
+
+}
+
+long double distance_calculation(long double lat1, long double lat2, long double long1, long double long2){
+
+
+}
+
---
+++ distance_calculation/DistanceCalculation.h
@@ -0,0 +1,9 @@
+#ifndef DISTANCECALCULATION_H
+#define DISTANCECALCULATION_H
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
+long double to_rad(long double degree);
+long double distance_calculation(long double lat1, long double lat2, long double long1, long double long2);
+#endif
