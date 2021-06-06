17c1103f42517726635e63bc9f13e782c951d3b0
Loay Elshall <Loay.elshall@icloud.com>
2021-06-06 14:09:31 +0200

    UART 1 init and helper functions

--- UART/uart.c
+++ UART/uart.c
@@ -1,2 +1,30 @@
 #include "uart.h"
 
+/****************** UART1 *************/
+void UART1_Init(void){
+  SYSCTL_RCGC1_R |= SYSCTL_RCGC1_UART1; // activate UART1
+  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOC; // activate port C
+  UART1_CTL_R &= ~UART_CTL_UARTEN;      // disable UART
+  UART1_IBRD_R = 520;  // 80,000,000/(16*115,200)) = 43.40278
+  UART1_FBRD_R = 53;  //6-bbit fraction, round(0.40278 * 64) = 26
+                                        // 8 bit word length (no parity bits, one stop bit, FIFOs)
+  UART1_LCRH_R = (UART_LCRH_WLEN_8|UART_LCRH_FEN);
+  UART1_CTL_R |= UART_CTL_UARTEN;       // enable UART
+  GPIO_PORTC_AFSEL_R |= 0x30;           // enable alt funct on PC5-4
+  GPIO_PORTC_DEN_R |= 0x30;             // enable digital I/O on PC5-4
+                                        // configure PC5-4 as UART1
+  GPIO_PORTC_PCTL_R = (GPIO_PORTC_PCTL_R&0xFF00FFFF)+0x00220000;
+  GPIO_PORTC_AMSEL_R &= ~0x30;          // disable analog functionality on PC5-4
+}
+
+unsigned char UART1_InChar(void){
+  while((UART1_FR_R&UART_FR_RXFE) != 0);
+  return((unsigned char)(UART1_DR_R&0xFF));
+}
+
+void UART1_OutChar(unsigned char data){
+  while((UART1_FR_R&UART_FR_TXFF) != 0);
+  UART1_DR_R = data;
+}
+
+
--- UART/uart.h
+++ UART/uart.h
@@ -1,6 +1,19 @@
 #ifndef _UART_H_
-#define _UART_H_                   // Device header
-#include "stdint.h"               // Device header
+#define _UART_H_
+
+// U1Rx connected to PC4
+// U1Tx connected to PC5
+
+#include "stdint.h"               
 #include <tm4c123gh6pm.h>
+
+
+/******** UART1 **********/
+void UART1_Init(void);
+
+unsigned char UART1_InChar(void);
+
+void UART1_OutChar(unsigned char data);
+
 
 #endif
