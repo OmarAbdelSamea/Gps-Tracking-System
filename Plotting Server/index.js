56758c41a051ed2f5b7906821ced565ce0946182
Loay Elshall <Loay.elshall@icloud.com>
2021-06-14 00:20:31 +0200

    server final fix

--- Plotting Server/index.js
+++ Plotting Server/index.js
@@ -1,11 +1,11 @@
-var mymap = L.map('mapid').setView([30.01578, 31.1698], 13);
+var mymap = L.map('mapid').setView([30.06570, 31.27832], 17);
 
-let lastLat = "30.00157883";
-let lastLong = "31.16980733";
+let lastLat = 0;
+let lastLong = 0;
 let connected = 0;
 let lastUpdate = 0;
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibG9heWVsc2hhbGwiLCJhIjoiY2twdG0wZTdkMGRwdTJ3cXd5Nzg1MnNscyJ9.eLOTtKZhCSbzkWboxjS4cg', {
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -33,6 +33,9 @@ function checkConnection(){
         lastUpdated.innerHTML = `Last Updated: <b>${lastUpdate} secs ago</b>`;
     } 
     lastUpdate++;  
+    if(lastUpdate > 30){
+        connected = 0;
+    }
 }
 
 function updateDistance(newDistance){
@@ -49,6 +52,10 @@ const updateUI = async ()=>{
         updateDistance(allData.distance);
         connected = 1;
         lastUpdate = 0;
+        if(lastLong == 0){
+            lastLong = allData.long;
+            lastLat = allData.lat;
+        }
         drawLine(lastLat, lastLong, allData.lat, allData.long);
         lastLat = allData.lat;
         lastLong = allData.long;
